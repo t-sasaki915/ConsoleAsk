a19ea5220908ac5dd263097f1853847980080897
Toma Sasaki <netst915@gmail.com>
2025-06-22 12:17:20 +0900

    ask monad

--- src/System/Console/Ask.hs
+++ src/System/Console/Ask.hs
@@ -1,8 +1,33 @@
 module System.Console.Ask
     ( NewlineTiming (..)
     , Behaviour (..)
+    , Ask (..)
     , defaultBehaviour
+    , runAsk
     ) where
 
+import           Control.Monad                (join)
+import           Control.Monad.IO.Class       (MonadIO (..))
 import           System.Console.Ask.Behaviour
-import           System.Console.Ask.Internal
+
+data Ask a = Ask { runAsk' :: Behaviour -> IO a }
+
+runAsk :: Behaviour -> Ask a -> IO a
+runAsk = flip runAsk'
+
+instance Functor Ask where
+    fmap f ma = ma >>= return . f
+
+instance Applicative Ask where
+    pure a = Ask { runAsk' = const (pure a) }
+
+    liftA2 f ma mb = do
+        a <- ma
+        b <- mb
+        return (f a b)
+
+instance Monad Ask where
+    (>>=) fa f = Ask { runAsk' = \behaviour -> runAsk behaviour (join (liftIO (f <$> runAsk behaviour fa))) }
+
+instance MonadIO Ask where
+    liftIO ma = Ask { runAsk' = const ma }
