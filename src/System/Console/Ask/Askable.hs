1cabe6c9f8be04e282c82fdd0016682511954bd6
Toma Sasaki <netst915@gmail.com>
2025-06-30 22:01:43 +0900

    add docs for askable

--- src/System/Console/Ask/Askable.hs
+++ src/System/Console/Ask/Askable.hs
@@ -1,3 +1,14 @@
+{-|
+Module      : System.Console.Ask.Askable
+Copyright   : 2025 Toma Sasaki
+Licence     : MIT
+Maintainer  : netst915@gmail.com
+Portability : Portable
+
+@System.Console.Ask.Askable@ provides 'Askable' and 'fromParsec'.
+You should import this module if you want to do 'ask' for your original data types.
+-}
+
 {-# LANGUAGE FlexibleInstances #-}
 
 module System.Console.Ask.Askable
@@ -11,9 +22,35 @@ import           Text.Parsec     (Parsec, anyChar, eof, parse)
 import           Text.Read       (readMaybe)
 import           Text.Regex.TDFA ((=~))
 
+-- | 'Askable' provides 'fromText'.
+-- Instances of 'Askable' must derive @Show@.
+-- This typeclass is used when 'ask', 'askOptional' and 'askOrElse' try to parse user input.
+-- Implementing 'fromText' is required.
+--
 class Show a => Askable a where
+    -- | 'fromText' converts @Text@ into a certain type.
+    -- Returning @Nothing@ makes the conversion failed.
+    --
+    -- @
+    -- data EmailAddress = EmailAddress Text deriving Show
+    --
+    -- instance 'Askable' EmailAddress where
+    --     'fromText' text =
+    --         if text =~ ("[a-zA-Z0-9+._-]+@[a-zA-Z-]+\\.[a-z]+" :: Text)
+    --             then Just (EmailAddress text)
+    --             else Nothing
+    -- @
+    --
     fromText :: Text -> Maybe a
 
+-- | 'fromParsec' converts @Parsec Text () a@ into @Text -> Maybe a@.
+-- It is supposed to be used with 'fromText'.
+--
+-- @
+-- instance 'Askable' 'Char' where
+--     'fromText' = 'fromParsec' (anyChar <* eof)
+-- @
+--
 fromParsec :: Parsec Text () a -> Text -> Maybe a
 fromParsec parser = either (const Nothing) Just . parse parser ""
 
